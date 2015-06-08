b15cfcc9c82464b5ec2f23c396c6d18b79434659
alex-erygin <alex.erygin@gmail.com>
2015-06-08 19:02:54 +0300

    IntegrityCheck used

--- msbuild-teamcity/project/Source/ConsoleApp/Program.cs
+++ msbuild-teamcity/project/Source/ConsoleApp/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace ConsoleApp
+﻿using System.IO;
+using System.Reflection;
+using Infotecs.RedAlert;
+
+namespace ConsoleApp
 {
     public class Program
     {
         private static void Main(string[] args)
         {
+            var crgPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "List.prg");
+            IntegrityChecker.CheckIntegrity(crgPath);
             new MyProgram().SayHello();
         }
     }
