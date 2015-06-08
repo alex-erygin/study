ea4ab08bedb55475f6471ddd73a96b3fc9aa2506
alex-erygin <alex.erygin@gmail.com>
2015-06-08 18:41:39 +0300

    Фикс после изменения настроек сборки

--- msbuild-teamcity/project/build.bat
+++ msbuild-teamcity/project/build.bat
@@ -1,3 +1,3 @@
-msbuild /p:Platform="Any CPU" build.msbuild /p:Configuration="Release"
-msbuild /p:Platform="Any CPU" UnitTests.msbuild /p:Configuration="Release"
-msbuild /p:Platform="Any CPU" IntegrityCheckCreator.msbuild /p:Configuration="Release"
+msbuild /p:Platform="x86" build.msbuild /p:Configuration="Release"
+msbuild /p:Platform="x86" UnitTests.msbuild /p:Configuration="Release"
+msbuild /p:Platform="x86" IntegrityCheckCreator.msbuild /p:Configuration="Release"
