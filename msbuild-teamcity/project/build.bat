8c17f33504795be0bc7e24781a97120797eb6354
alex-erygin <alex.erygin@gmail.com>
2015-06-08 18:48:43 +0300

    Prepare setup

--- msbuild-teamcity/project/build.bat
+++ msbuild-teamcity/project/build.bat
@@ -1,3 +1,4 @@
 msbuild /p:Platform="x86" build.msbuild /p:Configuration="Release"
 msbuild /p:Platform="x86" UnitTests.msbuild /p:Configuration="Release"
 msbuild /p:Platform="x86" IntegrityCheckCreator.msbuild /p:Configuration="Release"
+msbuild /p:Platform="x86" PrepareSetup.msbuild /p:Configuration="Release"
