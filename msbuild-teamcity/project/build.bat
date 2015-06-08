bd5855710e6c825977468a43fdef07e5478d4791
alex-erygin <alex.erygin@gmail.com>
2015-06-08 16:44:45 +0300

    Integrity check msbuild

--- msbuild-teamcity/project/build.bat
+++ msbuild-teamcity/project/build.bat
@@ -1,2 +1,3 @@
 msbuild /p:Platform="Any CPU" build.msbuild /p:Configuration="Release"
 msbuild /p:Platform="Any CPU" UnitTests.msbuild /p:Configuration="Release"
+msbuild /p:Platform="Any CPU" IntegrityCheckCreator.msbuild /p:Configuration="Release"
