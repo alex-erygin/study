aaa6ee41c44dff8406863a9a085d8b62175d8177
alex-erygin <alex.erygin@gmail.com>
2015-08-14 17:49:09 +0300

    +Список авторов

---
+++ asp.net mvc/play/webapp/HtmlHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace webapp
+{
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlString HtmlConvertToJson(this HtmlHelper htmlHelper, object model)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Formatting = Formatting.Indented
+            };
+
+            return new HtmlString(JsonConvert.SerializeObject(model, settings));
+        }
+    }
+}
