8b8436e2094b7477f65d7fd943f7e079c67b535a
alex-erygin <alex.erygin@gmail.com>
2015-09-03 09:48:56 +0300

    Отображение коментов

--- asp.net mvc/app/viewModel.js
+++ asp.net mvc/app/viewModel.js
@@ -8,33 +8,52 @@ var BlogViewModel = function() {
 		var title = ko.observable();
 		var description = ko.observable();
 		var content = ko.observable();
-		var comments = ko.observableArray()
+		var comments = ko.observableArray();
 	};
 
+	var Comment = function() {
+		var content = ko.observable();
+		var date = ko.observable();
+	};	
 	
+	function getDate() {
+		return new Date().toJSON().slice(0,10);
+	};
+
 	function Init() {
 		for (var i = 1; i <= 10; i++) {
 			var post = new Post();
-			post.date = new Date().toJSON().slice(0,10);
+			post.date = getDate();
 			post.title = "Пост №" + i;
 			post.description = "Этот пост о " + i;
 			post.content = i;
-			
+			post.comments = ko.observableArray();
 			self.posts.push(post);
-		};
+		}
+
+		self.posts().forEach(function(item){
+			var comment = new Comment();
+			comment.content = "Наказать жестачайшэ" + item.content;
+			comment.date = getDate();
+			item.comments.push(comment);			
+
+			var comment = new Comment();
+			comment.content = "Аффтор жжот! " + item.content;
+			comment.date = getDate();
+			item.comments.push(comment);			
+		});
 	};
 
-	Init();
-
-	function SelectPost(blogPost){
+	function SelectPost(blogPost) {
 		self.selectedPost(blogPost);
 		console.log(self.selectedPost());
 	}
 
+	Init();
 
 	return {
 		posts : self.posts,
 		selectPost : SelectPost,
 		selectedPost : self.selectedPost
 	};
-}
+};
