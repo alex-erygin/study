78b0526b627ef2dd5ee1a54d518d094ed0ad7248
Alex Erygin <alex.erygin@gmail.com>
2015-09-16 00:55:29 +0300

    security critical console app

---
+++ CodeAccessSecurity/Samples/SecurityCriticalConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security;
+
+//Коду SecurityCritical можно все
+[assembly: SecurityCritical]
+namespace SecurityCriticalConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//все сработает, потомучто мы - SecurityCritical!.
+			uint ui = GetVersion();
+			Console.ReadKey(true);
+		}
+
+		[DllImport("kernel32.dll")]
+		static extern uint GetVersion();
+	}
+}
