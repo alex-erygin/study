57b95a075cc7ee39ae769385971c5313181ac8ee
alex-erygin <alex.erygin@gmail.com>
2015-09-28 12:26:23 +0300

    DPAPI demo first commit

---
+++ data-protection-api/UnitTests/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Xunit;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        [Fact]
+        void ProtectInMemoryData_ShouldBeProtected()
+        {
+            var data = Encoding.UTF8.GetBytes("Your secret pass");
+
+            Console.WriteLine($"Original data: {Encoding.UTF8.GetString(data)}");
+
+            ProtectedMemory.Protect(data, MemoryProtectionScope.SameProcess);
+            Console.WriteLine($"Protected data: {Encoding.UTF8.GetString(data)}");
+
+            ProtectedMemory.Unprotect(data, MemoryProtectionScope.SameProcess);
+            Console.WriteLine($"Unprotected data: {Encoding.UTF8.GetString(data)}");
+        }
+    }
+}
