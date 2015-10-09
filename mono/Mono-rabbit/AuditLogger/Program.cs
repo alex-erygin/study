e4d6c2d626c7b8c69bacdabccf927416dd0d7c06
alex-erygin <alex.erygin@gmail.com>
2015-10-09 19:04:08 +0300

    rabbit-mono

---
+++ mono/Mono-rabbit/AuditLogger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.MessagePatterns;
+
+namespace AuditLogger
+{
+	class MainClass
+	{
+		private const string QueueName = "haba-haba";
+
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+
+			using (var connection = factory.CreateConnection())
+			{
+				using (var channel = connection.CreateModel())
+				{
+					Subscription subscription = new Subscription(channel, QueueName);
+					foreach (var eventArgs in subscription)
+					{
+						var senderId = new Guid(((BasicDeliverEventArgs)eventArgs).Body);
+						Console.WriteLine("Пришло сообщение от {0}", senderId);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ mono/Mono-rabbit/NCCService/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Threading;
+
+namespace NCCService
+{
+	class MainClass
+	{
+		private const string QueueName = "haba-haba";
+
+		public static void Main (string[] args)
+		{
+			var publisherId = Guid.NewGuid();
+			Console.WriteLine("Я - отправитель {0}", publisherId);
+
+			var factory = new ConnectionFactory { HostName = "localhost" };
+			using (var connection = factory.CreateConnection())
+			{
+				using (var channel = connection.CreateModel())
+				{
+					channel.QueueDeclare(QueueName, false, false, false, null);
+					var msg = publisherId.ToByteArray();
+
+					for (;;)
+					{
+						Thread.Sleep(TimeSpan.FromSeconds(1));
+
+						channel.BasicPublish(string.Empty, QueueName, null, msg);
+						Console.WriteLine("Сообщение отправлено");
+					}
+				}
+			}
+		}
+	}
+}
