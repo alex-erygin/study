33d5f57d68c4c9637ffe7366785b9e1ddf8fdcd6
Alex Erygin <alex.erygin@gmail.com>
2015-10-17 15:23:58 +0300

    +interaction with thrift

--- nginx asp.net web api/source/web-api-app/Controllers/PatientsController.cs
+++ nginx asp.net web api/source/web-api-app/Controllers/PatientsController.cs
@@ -1,7 +1,13 @@
-﻿using System.Net;
+﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
 using System.Web.Http;
+using Common.ThriftInterface;
 using NLog;
+using Thrift.Protocol;
+using Thrift.Transport;
 
 namespace Eon.Kiosk.Service.Controllers
 {
@@ -13,7 +19,41 @@ namespace Eon.Kiosk.Service.Controllers
         public HttpResponseMessage Test()
         {
 			logger.Debug("Qwerty");
-            return Request.CreateResponse(HttpStatusCode.OK, "The service works!");
+			SayHelloToThrift();
+	        return Request.CreateResponse(HttpStatusCode.OK, "The service works!");
         }
+
+	    private void SayHelloToThrift()
+	    {
+			var transport = new TSocket("localhost", 9090);
+			var protocol = new TBinaryProtocol(transport);
+			var client = new LibraryService.Client(protocol);
+			transport.Timeout = 100;
+
+			try
+			{
+				transport.Open();
+
+				var allBooks = client.GetAllBooks(); // Thrift call
+
+				Console.WriteLine("Total number of books: {0}\n", allBooks.Count);
+
+				if (allBooks.Count > 0)
+				{
+					Console.Write("Getting the first book: ");
+					var firstBook = client.GetBook(allBooks.First().Id); // Thrift call
+
+					Console.WriteLine("Id: {0}, {1} by {2}", firstBook.Id, firstBook.Title, firstBook.Author);
+				}
+			}
+			catch (SocketException e)
+			{
+				Console.WriteLine("Could not connect to the server: {0}.", e.Message);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("An error occured: {0}", e.Message);
+			}
+		}
     }
 }
