09166427481d3b3527b3a813bb5b71d1bc16cd05
Alex Erygin <alex.erygin@gmail.com>
2015-11-08 15:01:47 +0300

    direct initialization example explicit constructor example

--- cpp/Constructors/Constructors/Constructors.cpp
+++ cpp/Constructors/Constructors/Constructors.cpp
@@ -46,9 +46,37 @@ private:
 	Contained3 c3;
 };
 
+
+class Box {
+public:
+	//direct member initialization, which is more efficient than using assignment operators inside the constructor body
+	Box(int width, int length, int height) 
+		: _width(width), _height(height), _length(length) 
+	{ }
+
+	//explicit constructor;
+	Box(int size) : _width(size), _height(size), _length(size)
+	{ }
+
+	int volume() {
+		return _width*_height*_length;
+	}
+private:
+	int _width;
+	int _height;
+	int _length;
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	//see order of construction.
 	DerivedContainer d;
+
+	Box b(2, 2, 2);
+	cout << "Box volume is " << b.volume() << endl;
+
+	//explicit constructor call
+	Box b2 = 3;
+	cout << "Box volume is " << b2.volume() << endl;
 	return 0;
 }
