2d1b9d3684b1333fb8378bcb903008e9372edd75
Aleksandr Erygin <alex.erygin@gmail.com>
2015-11-15 19:16:09 +0300

    no message

---
+++ C/HelloWorld/HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Александр Ерыгин on 15.11.15.
+//  Copyright © 2015 Alex Erygin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
