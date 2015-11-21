a661ec1d96d1b8e43ae38c4ce7957446ce1c0d19
Aleksandr Erygin <alex.erygin@gmail.com>
2015-11-22 01:08:25 +0300

    getchar-putchar

---
+++ C/getchar-putchar/getchar-putchar/main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  getchar-putchar
+//
+//  Created by Александр Ерыгин on 22.11.15.
+//  Copyright © 2015 Alex Erygin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    char c;
+    
+    while((c = getchar()) != EOF){
+        if(c == '\t'){
+            printf("\\t");
+        }
+        else if(c == '\b'){
+            printf("\\b");
+        }
+        else if(c == '/'){
+            printf("\\");
+        }
+        else{
+            putchar(c);
+        }
+
+    }
+    
+    return 0;
+}
