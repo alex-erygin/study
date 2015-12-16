7d29004dab051ef84bd00d1f79c174c80c109cb4
alex-erygin <alex.erygin@gmail.com>
2015-12-16 19:58:20 +0300

    black magic begins

--- cpp/sign-with-cert/sources/SignLib/SignLib.cpp
+++ cpp/sign-with-cert/sources/SignLib/SignLib.cpp
@@ -4,6 +4,18 @@
 
 #include "SignLib.h"
 
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include <wincrypt.h>
+
+#define CERT_PERSONAL_STORE_NAME  L"My"
+#define CERT_OTHER_PEOPLE_STORE_NAME L"AddressBook"
+#define MY_TYPE  (PKCS_7_ASN_ENCODING | X509_ASN_ENCODING)
+#define BUFSIZE 1024
+
+#pragma comment(lib, "crypt32.lib")
+
 void SignLib::Signer::Sign(System::String^ signerName, System::String^ fileName, System::String^ signatureFileName)
 {
 	System::Console::WriteLine("Sign");
@@ -12,4 +24,32 @@ void SignLib::Signer::Sign(System::String^ signerName, System::String^ fileName,
 void SignLib::Signer::Verify(System::String^ signerName, System::String^ signatureFileName, System::String^ dataFileName)
 {
 	System::Console::WriteLine("Verify");
+}
+
+void SignLib::Signer::Sign()
+{
+     HCERTSTORE hStorehandle = NULL;
+     PCCERT_CONTEXT pSignerCert = NULL;
+     HCRYPTPROV hCryptoProv = NULL;
+     DWORD dsKeySpec = 0;
+     HCRYPTHASH hHash = NULL;
+     HANDLE hDataFile = NULL;
+     BOOL bResult = FALSE;
+     BYTE rgbFile[BUFSIZE];
+     DWORD cbRead = 0;
+     DWORD dwSigLen = 0;
+     BYTE* pbSignature = NULL;
+     HANDLE hSignatureFile = NULL;
+     DWORD lpNumberOfBytesWritten = 0;
+
+     System::Console::WriteLine("Signing");
+
+     //open certificate store
+     hStorehandle = ::CertOpenStore(
+          CERT_STORE_PROV_SYSTEM,
+          0,
+          NULL,
+          CERT_SYSTEM_STORE_CURRENT_USER, CERT_PERSONAL_STORE_NAME);
+
+
 }
--- cpp/sign-with-cert/sources/SignLib/SignLib.h
+++ cpp/sign-with-cert/sources/SignLib/SignLib.h
@@ -2,12 +2,7 @@
 
 #pragma once
 
-#define CERT_PERSONAL_STORE_NAME  L”My”
-#define CERT_OTHER_PEOPLE_STORE_NAME L”AddressBook”
-#define MY_TYPE  (PKCS_7_ASN_ENCODING | X509_ASN_ENCODING)
-#define BUFSIZE 1024
 
-using namespace System;
 
 namespace SignLib {
 
@@ -17,5 +12,8 @@ namespace SignLib {
 	public:
           void Sign(System::String^ signerName, System::String^ fileName, System::String^ signatureFileName);
 		void Verify(System::String^ signerName, System::String^ signatureFileName, System::String^ dataFileName);
+
+     private:
+          void Sign();
 	};
 }
