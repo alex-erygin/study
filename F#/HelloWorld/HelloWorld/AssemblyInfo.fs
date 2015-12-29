3f9513ea2cde7ec5c61aa088ac13de8523b68437
alex-erygin <alex.erygin@gmail.com>
2015-12-29 15:45:14 +0300

    F# Hello world

---
+++ F#/HelloWorld/HelloWorld/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace HelloWorld.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// Общие сведения о сборке можно задать с помощью следующего 
+// набора атрибутов. Отредактируйте эти значения атрибутов, чтобы изменить сведения,
+// связанные с этой сборкой.
+[<assembly: AssemblyTitle("HelloWorld")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("InfoTeCS")>]
+[<assembly: AssemblyProduct("HelloWorld")>]
+[<assembly: AssemblyCopyright("Copyright © InfoTeCS 2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// При установке значения False в атрибуте ComVisible типы в этой сборке становятся невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте для атрибута ComVisible значение True для этого типа.
+[<assembly: ComVisible(false)>]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект видим для COM
+[<assembly: Guid("fdea7659-9c57-4c1f-afa4-775afe00886a")>]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+// 
+//       Основной номер версии
+//       Дополнительный номер версии 
+//       Номер сборки
+//      Редакция
+// 
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [<сборка: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ F#/HelloWorld/HelloWorld/Program.fs
@@ -0,0 +1,11 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.org
+// Дополнительную справку см. в проекте "Учебник по F#".
+
+let SayHello() =
+    printfn "Hello"
+
+[<EntryPoint>]
+let main argv = 
+    SayHello()
+    System.Console.ReadKey() |> ignore
+    0 // возвращение целочисленного кода выхода
