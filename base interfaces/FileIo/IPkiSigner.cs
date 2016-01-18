ed2404e5de87c6dbd9223641bd86a39098f961c3
alex-erygin <alex.erygin@gmail.com>
2016-01-18 10:56:29 +0300

    +IPkiSigner

---
+++ base interfaces/FileIo/IPkiSigner.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace FileIo
+{
+    /// <summary>
+    /// Подписывает файлы с помощью одного или нескольких сертификатов.
+    /// </summary>
+    [ComImport]
+    [Guid("A042B1E2-6984-4F4A-A4D7-7612936E9849")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IPkiSigner
+    {
+        /// <summary>
+        /// Подписать.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        [PreserveSig]
+        void Sign();
+    }
+}
---
+++ base interfaces/FileIo/NativeMethods.cs
@@ -0,0 +1,7 @@
+﻿namespace FileIo
+{
+    internal static class NativeMethods
+    {
+        public static extern int CreateSigner(IFile inputFile, IFile outputFile, string CN, string serialNumber);
+    }
+}
