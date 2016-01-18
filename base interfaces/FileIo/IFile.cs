8b1fb01d3d71b44c779d43665b0d5b8687b3a630
alex-erygin <alex.erygin@gmail.com>
2016-01-18 10:39:49 +0300

    Базовый интерфейс для чтения и записи в файл.

---
+++ base interfaces/FileIo/IFile.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace FileIo
+{
+    [ComImport]
+    [Guid("E904BF6A-AEF3-4BA1-87F6-CF34150132EC")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IFile
+    {
+        /// <summary>
+        /// Прочесть данные в  буфер.
+        /// </summary>
+        /// <param name="buffer">Буфер, в который будут считаны данные.</param>
+        /// <param name="index">Индекс, с которого данные будут записаны в буфер.</param>
+        /// <param name="count">Количество байт, которые необходимо считать.</param>
+        /// <returns>Количество фактически считанных байт.</returns>
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        [PreserveSig]
+        int Read(
+            [In][Out][MarshalAs(UnmanagedType.LPArray, SizeParamIndex=2)] byte[] buffer, 
+            [In] int index, 
+            [In] int count);
+
+        /// <summary>
+        /// Записать буффер в файл.
+        /// </summary>
+        /// <param name="buffer">Буффер.</param>
+        /// <param name="index">Индекс в буффере, начиная с которого данные должны быть записаны.</param>
+        /// <param name="count">Количество байт, которые необходимо записать в файл.</param>
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        [PreserveSig]
+        void Write(
+            byte[] buffer,
+            int index,
+            int count
+            );
+    }
+}
---
+++ base interfaces/FileIo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("FileIo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("InfoTeCS")]
+[assembly: AssemblyProduct("FileIo")]
+[assembly: AssemblyCopyright("Copyright © InfoTeCS 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("4dfb5416-971c-4e32-b5d9-fcfe8157b473")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
