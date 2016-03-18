1d07dc8a52e957fb5765d71727360c312eb949e0
alex-erygin <alex.erygin@gmail.com>
2016-03-18 14:32:58 +0300

    hulio Displays stackoverflow reputation

--- telegram/source/huliobot/Program.cs
+++ telegram/source/huliobot/Program.cs
@@ -1,45 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Xml.Linq;
+using NLog;
+using SafeConfig;
 using Telegram.Bot;
+using Telegram.Bot.Types;
+
+// ReSharper disable All
 
 namespace huliobot
 {
     /// <summary>
-    /// Бот по имени Хулио.
+    ///     Бот по имени Хулио.
     /// </summary>
     public class Program
     {
         private static void Main(string[] args)
         {
-            Run().Wait();
-        }
-
-        private static async Task Run()
-        {
-            var token = SettingsStore.Tokens["hulio-token"];
-            var hulio = new Api(token);
-            var me = await hulio.GetMe();
-            Console.WriteLine($"{me.Username} на связи");
+            HulioBot bot = new HulioBot();
+            bot.Run().Wait();
         }
     }
 
-    public class SettingsStore
+    public class HulioBot
     {
-        public static readonly Dictionary<string, string> Tokens = new Dictionary<string, string>();
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        static SettingsStore()
+        private readonly Dictionary<string, Action<Api, Update>> commandHandlers =
+            new Dictionary<string, Action<Api, Update>>();
+
+        private ConfigManager configManager;
+        private int offset;
+
+        public HulioBot()
         {
-            var settings = XDocument.Parse(File.ReadAllText(@"SecretSettings.xml"));
-            foreach (var setting in settings.Root.Elements())
+            commandHandlers[Commands.Statistics] = OnFm;
+
+            configManager = new ConfigManager().WithCurrentUserScope().Load();
+
+            offset = configManager.Get<int>(nameof(offset));
+        }
+
+        private async void OnFm(Api api, Update update)
+        {
+            //get rep on stackoverflow
+
+            HttpClient client = new HttpClient();
+            string page = await client.GetStringAsync(@"http://stackoverflow.com/users/1549113/alex-erygin");
+            Regex regex = new Regex(@"title=""reputation"">\s+(?<rep>\d{1,6})\s+<span");
+            MatchCollection matches = regex.Matches(page);
+            Match match = matches.Cast<Match>().Where(x => x.Success).FirstOrDefault();
+            if (match != null)
             {
-                Tokens[setting.Attribute("key").Value] = setting.Attribute("value").Value;
+                await api.SendTextMessage(update.Message.Chat.Id, $"stack: {match.Groups["rep"].Value}");
             }
         }
-        
-        
+
+        public async Task Run()
+        {
+            try
+            {
+                string token = SettingsStore.Tokens["hulio-token"];
+                Api bot = new Api(token);
+                User me = await bot.GetMe();
+                Logger.Debug($"{me.Username} на связи");
+
+                int offset = configManager.Load().Get<int>("offset");
+                while (true)
+                {
+                    Update[] updates = await bot.GetUpdates(offset);
+
+                    foreach (Update update in updates)
+                    {
+                        switch (update.Message.Type)
+                        {
+                            case MessageType.TextMessage:
+                            {
+                                if (commandHandlers.ContainsKey(update.Message.Text))
+                                {
+                                    commandHandlers[update.Message.Text](bot, update);
+                                }
+                            }
+                                break;
+                        }
+
+                        offset = update.Id + 1;
+                    }
+                    configManager.Set(nameof(offset), offset).Save();
+                    await Task.Delay(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                throw;
+            }
+        }
+
+        private class Commands
+        {
+            /// <summary>
+            ///     Выводит статистику.
+            /// </summary>
+            public static string Statistics => "/fm";
+        }
     }
 }
