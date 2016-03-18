4517735e5eb6e1818785036132e08b0baf56cc54
alex-erygin <alex.erygin@gmail.com>
2016-03-18 13:01:33 +0300

    Bot skeleton

---
+++ telegram/source/huliobot/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace huliobot
+{
+    /// <summary>
+    /// Бот по имени Хулио.
+    /// </summary>
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
