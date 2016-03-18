6b039d57bf71dd2d784d6576055f67fe8e05687e
alex-erygin <alex.erygin@gmail.com>
2016-03-18 13:24:55 +0300

    Бот на связи

--- telegram/source/huliobot/Program.cs
+++ telegram/source/huliobot/Program.cs
@@ -1,4 +1,12 @@
-﻿namespace huliobot
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Telegram.Bot;
+
+namespace huliobot
 {
     /// <summary>
     /// Бот по имени Хулио.
@@ -7,7 +15,31 @@
     {
         private static void Main(string[] args)
         {
-
+            Run().Wait();
         }
+
+        private static async Task Run()
+        {
+            var token = SettingsStore.Tokens["hulio-token"];
+            var hulio = new Api(token);
+            var me = await hulio.GetMe();
+            Console.WriteLine($"{me.Username} на связи");
+        }
+    }
+
+    public class SettingsStore
+    {
+        public static readonly Dictionary<string, string> Tokens = new Dictionary<string, string>();
+
+        static SettingsStore()
+        {
+            var settings = XDocument.Parse(File.ReadAllText(@"SecretSettings.xml"));
+            foreach (var setting in settings.Root.Elements())
+            {
+                Tokens[setting.Attribute("key").Value] = setting.Attribute("value").Value;
+            }
+        }
+        
+        
     }
 }
