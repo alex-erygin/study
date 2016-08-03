c93c1cc0d668a7c0cc6d7155d20976a5971fae99
Erygin Aleksandr <Erygin.Ale@infotecs-nt>
2016-08-03 10:51:29 +0300

    + create certificate store from powershell

---
+++ ps/create-store-addressbook.ps1
@@ -0,0 +1,3 @@
+﻿$store = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Store -ArgumentList @([System.Security.Cryptography.X509Certificates.StoreName]::AddressBook, [System.Security.Cryptography.X509Certificates.StoreLocation]::CurrentUser) 
+$store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
+$store.Close()
