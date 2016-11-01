0919ff6bf625aab046fac7b3cc74ce1ac14c333b
alex <alex@debian>
2016-11-01 11:06:28 -0400

    bare project

---
+++ C/zadachki/BitCounter.cpp
@@ -0,0 +1,8 @@
+//
+// Created by alex on 01/11/16.
+//
+
+#include "BitCounter.h"
+int BitCounter::CountBits(int number) {
+    return 0;
+}
---
+++ C/zadachki/BitCounter.h
@@ -0,0 +1,15 @@
+//
+// Created by alex on 01/11/16.
+//
+
+#ifndef ZADACHKI_BITCOUNTER_H
+#define ZADACHKI_BITCOUNTER_H
+
+
+class BitCounter {
+public:
+    int CountBits(int number);
+};
+
+
+#endif //ZADACHKI_BITCOUNTER_H
