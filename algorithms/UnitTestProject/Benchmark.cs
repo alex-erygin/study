8c5752dbba26c90b3a657d86633207ee9d1e05fd
Aleksandr Erygin <alex.erygin@gmail.com>
2017-09-07 18:27:56 +0300

    Алгоритмы: линейный конгруэнтный генератор

---
+++ algorithms/UnitTestProject/Benchmark.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace UnitTestProject
+{
+    public class Benchmark : IDisposable 
+    {
+        private readonly Stopwatch timer = new Stopwatch();
+        private readonly string benchmarkName;
+
+        public Benchmark(string benchmarkName)
+        {
+            this.benchmarkName = benchmarkName;
+            timer.Start();
+        }
+
+        public void Dispose() 
+        {
+            timer.Stop();
+            Console.WriteLine($"{benchmarkName} {timer.Elapsed}");
+        }
+    }
+}
