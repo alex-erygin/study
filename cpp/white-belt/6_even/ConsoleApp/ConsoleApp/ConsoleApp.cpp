5e51534a0472ee85aed23f033afcd616316c6bee
Aleksandr Erygin <alex.erygin@gmail.com>
2018-05-05 09:04:07 +0300

    cpp white belt 1st week solutions

---
+++ cpp/white-belt/6_even/ConsoleApp/ConsoleApp/ConsoleApp.cpp
@@ -0,0 +1,21 @@
+// ConsoleApp.cpp : Defines the entry point for the console application.
+//
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a,b;
+	
+	cin >> a >> b;
+
+	for(int i = a; i<=b; ++i)
+	{
+		if (i % 2 == 0)
+		{
+			cout << i << " ";
+		}
+	}
+
+    return 0;
+}
