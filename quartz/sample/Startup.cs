5dc99e50ff0c1375d28f9229bc8b1667938391cd
Aleksandr Erygin <a.erygin@atol.ru>
2020-03-09 23:00:57 +0300

    ADO (postgresql) job storage configured.

--- quartz/sample/Startup.cs
+++ quartz/sample/Startup.cs
@@ -1,3 +1,4 @@
+using System.Collections.Specialized;
 using CrystalQuartz.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -18,18 +19,11 @@ namespace sample
         {
             services.AddControllers();
 
-            var scheduler = StdSchedulerFactory.GetDefaultScheduler().GetAwaiter().GetResult();
-            
-            services.AddSingleton(scheduler);
+            RegisterDependencies(services);
 
-            services.AddHostedService<QuartzHostedService>();
-
-            // If using Kestrel:
-            services.Configure<KestrelServerOptions>(options =>
-            {
-                options.AllowSynchronousIO = true;
-            });
+            services.Configure<KestrelServerOptions>(options => options.AllowSynchronousIO = true);
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -44,6 +38,31 @@ namespace sample
 
             var scheduler = app.ApplicationServices.GetService<IScheduler>();
             app.UseCrystalQuartz(()=> scheduler);
+        }
+
+
+        private static void RegisterDependencies(IServiceCollection services)
+        {
+            var properties = new NameValueCollection
+            {
+                ["quartz.scheduler.instanceName"] = "TikTak",
+                ["quartz.scheduler.instanceId"] = "single_instance",
+                ["quartz.threadPool.threadCount"] = "2",
+                ["quartz.jobStore.type"] = "Quartz.Impl.AdoJobStore.JobStoreTX, Quartz",
+                ["quartz.jobStore.useProperties"] = "true",
+                ["quartz.jobStore.dataSource"] = "default",
+                ["quartz.serializer.type"] = "json",
+                ["quartz.jobStore.tablePrefix"] = "qrtz_",
+                ["quartz.dataSource.default.connectionString"] =
+                    "Server=127.0.0.1;Port=5432;Database=scheduler;Userid=postgres;Password=--;Pooling=true;MinPoolSize=1;MaxPoolSize=2;",
+                ["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.StdAdoDelegate, Quartz",
+                ["quartz.dataSource.default.provider"] = "Npgsql"
+            };
+
+            var factory = new StdSchedulerFactory(properties);
+            var scheduler = factory.GetScheduler().GetAwaiter().GetResult();
+            services.AddSingleton(scheduler);
+            services.AddHostedService<QuartzHostedService>();
         }
     }
 }
