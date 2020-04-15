241738de3f552930bb5fd7c6fb3ff0858da6c534
alex-erygin <alex.erygin@gmail.com>
2020-04-15 15:55:16 +0300

    Orleans hello world

---
+++ orleans/getting-started/Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using GrainInterfaces;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunMainAsync().Wait();
+        }
+
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                using (var client = await ConnectClient())
+                {
+                    await DoClientWork(client);
+                    Console.ReadKey();
+                }
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\nException while trying to run client: {ex.Message}");
+                Console.WriteLine("Make sure the silo the client is trying to connect to is running.");
+                Console.WriteLine("\nPress any key to exit.");
+                Console.ReadKey();
+                return 1;
+            }
+        }
+
+        private static async Task<IClusterClient> ConnectClient()
+        {
+            var client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "orleans-basics";
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .Build();
+
+            await client.Connect();
+            Console.WriteLine("Connected!");
+            return client;
+        }
+
+        private static async Task DoClientWork(IClusterClient client)
+        {
+            var friend = client.GetGrain<IHello>(0);
+            var response = await friend.SayHello("Good morning, HelloGrain!");
+            Console.WriteLine("\n\n{0}\n\n", response);
+        }
+    }
+}
