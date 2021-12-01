8c198b8448dcbc88349a213db406b8d45e7b7742
alex-erygin <alex.erygin@gmail.com>
2021-12-02 00:11:18 +0300

    https://docs.microsoft.com/ru-ru/learn/modules/fsharp-first-program/?ns-enrollment-type=LearningPath&ns-enrollment-id=learn.languages.fsharp-first-steps

---
+++ F#/Хранение и извлечение данных с помощью F#/app/app/Program.fs
@@ -0,0 +1,15 @@
+﻿
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Вводи значение, %%username%%"
+
+let str = System.Console.ReadLine()
+
+printfn "%s, %%username%%" str
+
+printfn "Сколько тебе лет, %%username%%?" 
+
+let ageString = System.Console.ReadLine()
+let age = int ageString
+
+printf "Тебе %i лет, старина" age
