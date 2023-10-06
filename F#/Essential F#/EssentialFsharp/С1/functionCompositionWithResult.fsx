2b60eae3998aedabbc99740b41a7a596b6dd4301
alex-erygin <alex.erygin@gmail.com>
2023-10-06 21:17:37 +0300

    Update functionCompositionWithResult.fsx

--- F#/Essential F#/EssentialFsharp/С1/functionCompositionWithResult.fsx
+++ F#/Essential F#/EssentialFsharp/С1/functionCompositionWithResult.fsx
@@ -6,6 +6,8 @@ type Customer = {
     Credit: decimal
 }
 
+
+
 let getPurchases customer =
     try
         // imagine this function is fetching data from a Database
@@ -21,25 +23,21 @@ let tryPromoteToVip purchases =
     if amount > 100M then { customer with IsVip = true }
     else customer
     
+    
 let increaseCreditIfVip customer =
     try
         // imagine this function is fetching data from a Database
-        let increase =
-            if customer.IsVip then 100.0M else 50.0M
+        let increase = if customer.IsVip then 100.0M else 50.0M
         Ok { customer with Credit = customer.Credit + increase }
     with
     | ex -> Error ex
-    
- 
- let upgradeCustomer customer =
-     customer
-     |> getPurchases
-     |> function
-        | Ok x->Ok(tryPromoteToVip x)
-        | Error ex -> Error ex
-     |> function
-        | Ok x -> increaseCreditIfVip x
-        | Error ex -> Error ex
+        
+let upgradeCustomer customer =
+    customer
+    |> getPurchases
+    |> Result.map tryPromoteToVip
+    |> Result.bind increaseCreditIfVip
+
 
 let customerVIP = { Id = 1; IsVip = true; Credit = 100.0M }
 let customerSTD = { Id = 2; IsVip = false; Credit = 100.0M }
